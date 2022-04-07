6d22fdfcd082565b3ffd72297de2b13e00db17db
AlexiusVictoria <lexyvt@gmail.com>
2022-04-06 22:49:23 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+import { connect } from 'react-redux'
+import { getQuote } from './actions/animeActions'
+
+
+const App = props => {
+
+  const fetchQuote = e => {
+    e.preventDefault();
+    props.getQuote()
+  }
+
   return (
     <div className="App">
-      Async Redux Project
+       <h1>Random Anime Quote</h1>
+       <div className='Anime-Container'>
+
+<p className='Anime-Title'>Anime: {props.anime}</p>
+<p className='Anime-Character'>Character: {props.character}</p>
+<label name='Anime-Quote'>Quote: </label>
+<p className='Anime-Quote' name='Anime-Quote'> {props.quote}</p>
+      <button onClick={fetchQuote}>Press for Quote!</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  anime: state.anime,
+  character: state.character,
+  quote: state.quote,
+  isFetching: state.isFetching,
+  error: state.error
+})
+
+export default connect(mapStateToProps, { getQuote })(App); 
